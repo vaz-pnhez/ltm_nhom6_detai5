344b9fceabb21e6d2b1c2cd677c94a7ba3bc6183
LSNr-tqn <tranquangnghia260806@gmail.com>
2026-04-20 17:15:25 +0700

    final 1.2.0

---
+++ Client/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static Socket client;
+        // Biến lưu chuỗi hiện tại người dùng nhập
+        static string currentInput = "";
+        static object consoleLock = new object();
+
+
+
+        static void Main()
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Nhập ip Server để kết nối
+            Console.Write("Nhập IP Server: ");
+            string ipStr = Console.ReadLine();
+            IPAddress ip = IPAddress.Parse(ipStr.Trim());
+            IPEndPoint endPoint = new IPEndPoint(ip, 5000);
+            client.Connect(endPoint);
+            Console.WriteLine("Đã kết nối Server!\n");
+
+            // Gửi tên cho Server
+            Console.Write("Nhập tên người dùng của bạn: ");
+            string name = Console.ReadLine();
+            client.Send(Encoding.UTF8.GetBytes(name));
+
+            // Bắt đầu Thread nhận tin
+            Thread receiveThread = new Thread(ReceiveMessage);
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+
+            // Bắt đầu gửi tin nhắn
+            while (true)
+            {
+                Console.Write("> ");
+                currentInput = "";
+
+                while (true)
+                {
+                    // Cho phép nhập liệu 1 ký tự
+                    var key = Console.ReadKey(true);
+
+                    // Nếu nhấn Enter sẽ gửi tin đi
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine();
+
+                        string name_msg = currentInput;
+                        // Gửi đi tên và message cho Server
+                        client.Send(Encoding.UTF8.GetBytes(name_msg));
+                        break;
+                    }
+                    // Nếu nhấn Backspace thì xoá đi 1 ký tự trong curentInput 
+                    else if (key.Key == ConsoleKey.Backspace)
+                    {
+                        if (currentInput.Length > 0)
+                        {
+                            currentInput = currentInput.Substring(0, currentInput.Length - 1);
+                            Console.Write("\b \b");
+                        }
+                    }
+                    // Nếu nhấn ký tự bình thường thì thêm ký tự đó vào curentInput và in ra màn hình
+                    else
+                    {
+                        currentInput += key.KeyChar;
+                        Console.Write(key.KeyChar);
+                    }
+                }
+            }
+        }
+
+        // Hàm Thread nhận tin nhắn
+        static void ReceiveMessage()
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (true)
+                {
+
+                    int received = client.Receive(buffer);
+
+                    // Xử lý khi Server ngắt kết nối
+                    if (received == 0)
+                    {
+                        lock (consoleLock)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("\n[Server đã ngắt kết nối!]");
+                        }
+                        break;
+                    }
+
+                    // Nhận lấy tin nhắn từ Client
+                    string msg = Encoding.UTF8.GetString(buffer, 0, received);
+
+                    // Tmj dừng thread
+                    lock (consoleLock)
+                    {
+                        // Xoá dòng hiện tại
+                        Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+
+                        // In tin nhắn
+                        Console.WriteLine(msg);
+
+                        // Viết lại dòng đang gõ dở
+                        Console.Write("> " + currentInput);
+                    }
+                }
+            }
+            // Xử lý Server ngắt kết nối
+            catch (SocketException)
+            {
+                lock (consoleLock)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\n[Server đã ngắt kết nối!]");
+                }
+            }
+            catch (Exception ex)
+            {
+                lock (consoleLock)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\n[Lỗi]: " + ex.Message);
+                }
+            }
+            finally
+            {
+                Console.WriteLine("\nNhấn bất kì để thoát...");
+                Console.ReadKey();
+                client.Close();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Linq;
+
+
+namespace ServerApplication
+{
+    public class Program
+    {
+        // Tạo dictionary để lưu tên giá 
+        static Dictionary<Socket, string> clientNames = new Dictionary<Socket, string>();
+        // Tạo List Client
+        static List<Socket> clients = new List<Socket>();
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            string ip = Dns.GetHostEntry(Dns.GetHostName())
+            .AddressList
+            .First(x => x.AddressFamily == AddressFamily.InterNetwork
+             && !IPAddress.IsLoopback(x))
+    .ToString();
+
+            Console.WriteLine("Server đang chạy tại: " + ip + ":5000");
+
+            // Khai bao dia chi ip
+            IPAddress ipadd = IPAddress.Any;
+            // tao mot end point
+            IPEndPoint ipend = new IPEndPoint(ipadd, 5000);
+
+            Socket skServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // thuc hien bin ipendpoint socket va lang nghe
+            skServer.Bind(ipend);
+            skServer.Listen(10);
+
+            Console.WriteLine("Server đang chờ Client...");
+            while (true)
+            {
+                Socket skClient = skServer.Accept();
+                clients.Add(skClient);
+
+                Console.WriteLine("Chấp nhận kết nối từ " + skClient.RemoteEndPoint);
+
+                Thread t = new Thread(HandleThread);
+                t.IsBackground = true;
+                t.Start(skClient);
+
+            }
+        }
+        static void HandleThread(object obj)
+        {
+            Socket client = (Socket)obj;
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+
+                // Nhận tên Client và thông báo tham gia
+                int recv = client.Receive(buffer);
+                string name = Encoding.UTF8.GetString(buffer, 0, recv);
+
+                clientNames[client] = name;
+
+                Console.WriteLine("\n> " + name + " đã tham gia\n");
+
+                Broadcast(name + " đã tham gia phòng chat", client);
+                while (true)
+                {
+                    recv = client.Receive(buffer);
+                    if (recv == 0) break;
+
+                    string msg = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine(name + ": " + msg);
+
+                    Broadcast(name + ": " + msg, client);
+                }
+            }
+            catch { }
+
+            // Khi client thoát
+            if (clientNames.ContainsKey(client))
+            {
+                string name = clientNames[client];
+                Console.WriteLine("\n< " + name + " đã rời\n");
+
+                Broadcast(name + " đã rời phòng chat", client);
+
+                clientNames.Remove(client);
+            }
+
+            clients.Remove(client);
+            client.Close();
+        }
+
+        static void Broadcast(string message, Socket sender)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            foreach (Socket client in clients)
+            {
+                if (client != sender)
+                {
+                    try
+                    {
+                        client.Send(data);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Lỗi khi gửi đến " + client.RemoteEndPoint + ": " + e.Message);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
